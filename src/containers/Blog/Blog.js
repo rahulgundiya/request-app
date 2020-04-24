5068c4f4afb7e0cd72760298a7645d7b39361cae
Rahul <rahulgundiya34@gmail.com>
2020-04-24 13:31:14 +0530

    All-Components

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+//import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import classes from './Blog.module.css';
+
+class Blog extends Component {
+    // componentDidMount()
+    // {
+    //     axios.get('http://jsonplaceholder.typicode.com/posts')
+    //     .then(response=>{
+    //         console.log(response);
+    //     })
+    // }
+    render () {
+        return (
+            <div>
+                <section className={classes.Posts}>
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
