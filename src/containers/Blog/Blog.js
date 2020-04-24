6078bf629ae559740e1aeed77c0e694036d36ccd
Rahul <rahulgundiya34@gmail.com>
2020-04-24 13:53:37 +0530

    Rendering Fetched Data to the

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,21 +6,28 @@ import NewPost from '../../components/NewPost/NewPost';
 import classes from './Blog.module.css';
 
 class Blog extends Component {
+    state={
+        Post:[]
+    }
     componentDidMount()
     {
         axios.get('http://jsonplaceholder.typicode.com/posts')
         .then(response=>{
-            console.log(response);
-            console.log('Hi');
+            this.setState({Post:response.data})
+           // console.log(response);
+            //console.log('Hi');
         })
     }
     render () {
+        const Posts = this.state.Post.map(post=>{
+
+            return <Post key={post.id} title={post.title} />
+        
+        })
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                   {Posts}
                 </section>
                 <section>
                     <FullPost />
